e509f646512fea1c2c4b8a2381cac5ed04952ccd
Cam <crodri93@depaul.edu>
2023-03-01 18:06:48 -0600

    crossing fingers
    updating

--- demo/demo-scripts/setupDB.py
+++ demo/demo-scripts/setupDB.py
@@ -15,19 +15,19 @@ downloads = {
   "districts22": "https://www2.census.gov/geo/tiger/TIGER2022/UNSD/tl_2022_47_unsd.zip" # 2020 unified school districts for tennessee
 }
 
-os.system("mkdir -p shapefiles/downloads")
+os.system("mkdir -p demo/shapefiles/downloads")
 for (table, url) in downloads.items():
   file = table + ".zip"
   # get shapefiles if they don't exist
-  if not os.path.exists("shapefiles/downloads/%s" % file):
+  if not os.path.exists("demo/shapefiles/downloads/%s" % file):
     print("Downloading %s file..." % table);
-    print("curl %s -o shapefiles/downloads/%s" % (url, file))
-    os.system("curl \"%s\" -o shapefiles/downloads/%s" % (url, file))
-    os.system("unzip shapefiles/downloads/%s -d shapefiles/%s" % (file, table))
+    print("curl %s -o demo/shapefiles/downloads/%s" % (url, file))
+    os.system("curl \"%s\" -o demo/shapefiles/downloads/%s" % (url, file))
+    os.system("unzip demo/shapefiles/downloads/%s -d demo/shapefiles/%s" % (file, table))
 
   # load into the DB
   # -D uses dump format
   # -I creates an index
-  shapefiles = [f for f in os.listdir("shapefiles/%s" % table) if os.path.splitext(f)[1] == ".shp"]
+  shapefiles = [f for f in os.listdir("demo/shapefiles/%s" % table) if os.path.splitext(f)[1] == ".shp"]
   shapefile = shapefiles[0]
-  os.system("shp2pgsql -DI -s %s shapefiles/%s/%s %s | psql dbname=demo " % (CRS, table, shapefile, table))
+  os.system("shp2pgsql -DI -s %s demo/shapefiles/%s/%s %s | psql dbname=demo " % (CRS, table, shapefile, table))
